7138908062c524b310b687ad2d8b5aff6b81f075
kiet019 <kietly019@gmail.com>
2023-05-30 15:33:39 +0700

    update

--- src/styles/Theme.tsx
+++ src/styles/Theme.tsx
@@ -1,11 +1,16 @@
 import { createTheme } from '@mui/material/styles';
 
 // Import các màu bạn muốn sử dụng
+
 const theme = createTheme({
   palette: {
     primary: {
       main: 'rgb(0 0 0)', // Mã màu chủ đạo mới
     },
+  },
+  typography: {
+    fontFamily: 'Roboto',
+    // Các tùy chỉnh kiểu chữ khác
   },
 });
 
