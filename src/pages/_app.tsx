779c675253b5ddfcf0075b740750eee6d1371b8a
kiet019 <kietly019@gmail.com>
2023-06-02 16:54:22 +0700

    update

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -6,11 +6,24 @@ import AuthProvider from "@/component/login/AuthContext";
 import AlertPopup from "@/component/AlertPopup";
 import { store } from "@/feature/ReduxStore";
 import { Provider } from "react-redux";
+import { NextSeo } from "next-seo";
 
 function MyApp({ Component, pageProps }: any) {
   return (
+    
     // 2. Use at the root of your app
     <Provider store={store}>
+      <NextSeo
+        title="TiemHommie"
+        description="Decoration and Gift"
+        openGraph={{
+          images: [
+            {
+              url: 'https://group-7-swp.vercel.app/assets/images/banner.jpg',
+            },
+          ],
+        }}
+      />
     <ThemeProvider theme={theme}>
       <AuthProvider>
       <AlertPopup>
