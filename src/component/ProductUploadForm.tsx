1fe1d6b36df4da59533daf6ec10113d749b11c7f
kiet <kietly019@gmail.com>
2023-06-04 14:41:25 +0700

    update

--- src/component/ProductUploadForm.tsx
+++ src/component/ProductUploadForm.tsx
@@ -190,6 +190,7 @@ export default function ProductUploadForm({ categoryList }: any) {
                     Upload Image
                   </Button>
                 </label>
+                <img src={createObjectURL} />
               </Grid>
               <Grid item xs={9}></Grid>
               <Grid item xs={3}>
